2be6943c23dd27289fdb36f2917d7df851aa03b6
Akhil Kumar <127279064+akhilkumar1101@users.noreply.github.com>
2026-09-16 12:28:31 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Maven Freestyle Project is working.");
+    }
+}
